2bd75eb093b66aea212ef9133a633bf17b888f76
giung <sko99@naver.com>
2025-02-01 17:01:56 +0900

    elastic search에 회의록 저장

--- congress_meeting/read_pdf_to_elastic_search.py
+++ congress_meeting/read_pdf_to_elastic_search.py
@@ -1,14 +1,27 @@
 import requests
 import pdfplumber
 import io
+import re
 from dotenv import load_dotenv
 import os
-from openai import OpenAI
-import re
+from elasticsearch import Elasticsearch
+from datetime import datetime
 
 load_dotenv()
-open_ai_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=open_ai_key)
+
+Elasticsearch_key = os.getenv("ELASTIC_SEARCH_PW")
+# Elasticsearch 연결
+es = Elasticsearch(
+    "https://localhost:9200",
+    basic_auth=("elastic", Elasticsearch_key),  # 인증 추가
+    verify_certs=False  # 자체 서명된 인증서 문제 방지
+    )
+
+# 인덱스 설정 (인덱스가 없으면 생성)
+index_name = "congress_meetings"
+
+if not es.indices.exists(index=index_name):
+    es.indices.create(index=index_name)
 
 def get_pdf_to_text(url):
     """ PDF URL에서 텍스트를 추출하는 함수 """
@@ -44,17 +57,26 @@ def get_speaker(text):
             "date": "2025-01-23",
             "speaker": speaker,
             "text": speech,
-            "summary": None  # 요약은 나중에 추가
+            "summary": None,  # 요약은 나중에 추가
+            'timestamp': datetime.now(),
         })
 
     print(f'분류 완료 (샘플 2개): {speech_list[:2]}')
     return speech_list
 
 # 이후에는 이 내용을 어떻게 저장할지 데이터베이스로 만들어야 함
+# elastic search를 이용해서 저장하면 될 거 같음
+
+def save_to_elasticsearch(data):
+    """ 추출된 국회의원 발언 데이터를 Elasticsearch에 저장하는 함수 """
+    for entry in data:
+        es.index(index=index_name, body=entry)
+    print("Elasticsearch 저장 완료!")
 
 if __name__ == "__main__":
     # PDF 다운로드 URL
     url = "http://likms.assembly.go.kr/record/mhs-10-040-0040.do?conferNum=054706&fileId=0000124454&deviceGubun=P&imsiYn=P"
 
     text = get_pdf_to_text(url)
-    get_speaker(text)
+    speech_data = get_speaker(text)
+    save_to_elasticsearch(speech_data)
