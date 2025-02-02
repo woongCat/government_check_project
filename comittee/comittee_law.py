b6c053858ad45038c0b313553b8a9fc62442e5f1
giung <sko99@naver.com>
2025-02-02 23:52:29 +0900

    위원회는 파일 이쪽으로 이동

---
+++ comittee/comittee_law.py
@@ -0,0 +1,3 @@
+
+# 위원회 계류 법률안
+url = "https://open.assembly.go.kr/portal/openapi/ndiwuqmpambgvnfsj"
---
+++ comittee/comittee_names.py
@@ -0,0 +1,3 @@
+
+# 위원회 명단
+url = "https://open.assembly.go.kr/portal/data/service/selectServicePage.do?infId=OCAJQ4001000LI18751&infSeq=1&isInfsPop=Y"
