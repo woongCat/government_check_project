f4cfad3aa381aaf8838f76e47eda1f2a84b06d0e
giung <sko99@naver.com>
2025-02-03 00:07:37 +0900

    구조 변경

---
+++ congress/main_meetings/get_meeting_schedule.py
@@ -0,0 +1,28 @@
+import requests
+from dotenv import load_dotenv
+import json
+import os
+
+# 나중에 airflow에 추가되야 하는 코드
+
+load_dotenv()
+key = os.getenv("OPEN_GOVERMETN_API_KEY")
+url = "https://open.assembly.go.kr/portal/openapi/nekcaiymatialqlxr"
+
+params = {
+    "KEY" : key,
+    "Type" : 'json',
+    "pIndex" : '1',
+    "pSize" : "100",
+    "UNIT_CD" : "100022",
+}
+
+# 요청 받기
+response = requests.get(url=url, params=params)
+
+# json으로 저장 받기
+file_path = 'congress/main_meetings/json/congress_schedule.json'
+with open(file_path, 'w') as f:
+    json.dump(response.json(), f)
+    
+print(response.json())
