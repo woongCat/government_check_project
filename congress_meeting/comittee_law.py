4306a769d2e4454e93d21a979114f7453c73d6ad
giung <sko99@naver.com>
2025-02-02 23:54:02 +0900

    파일바뀌면서 없어진 디렉토리 삭제

--- congress_meeting/comittee_law.py
+++
@@ -1,3 +0,0 @@
-
-# 위원회 계류 법률안
-url = "https://open.assembly.go.kr/portal/openapi/ndiwuqmpambgvnfsj"
--- congress_meeting/comittee_speak.py
+++
@@ -1,3 +0,0 @@
-
-# 위원회 회의록
-url = "https://open.assembly.go.kr/portal/openapi/ncwgseseafwbuheph"
